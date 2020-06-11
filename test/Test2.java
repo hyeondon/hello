43b580db58dd00adc481b97c29d86b78ef53b496
hyeondon <qpwlxm123@naver.com>
2020-06-11 17:29:23 +0900

    17:29

--- test/Test2.java
+++ test/Test2.java
@@ -1,18 +1,18 @@
-package rps;
+package test;
 
 import java.util.Scanner;
 
-public class WhileDemo{
+public class Test2{
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		int fage = scanner.nextInt();
 		while(true) {
-			int fage = scanner.nextInt();
-			System.out.println("메뉴 0.종료 1.시작");
+			System.out.println("메뉴 0.종료 1시작");
 			switch(fage) {
 			case 0 : System.out.println("게임종료");return;
-			case 1 : System.out.println("게임 중..");break;
+			case 1 : System.out.println("게임 중");break;
+			
 			}
 		}
 	}
 }
-
