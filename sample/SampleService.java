aed2b85ec93400669164f24121f0479c610b9ba8
hyeondon <qpwlxm123@naver.com>
2020-06-15 18:58:30 +0900

    18:58

--- sample/SampleService.java
+++ sample/SampleService.java
@@ -2,22 +2,28 @@ package sample;
 
 public interface SampleService {
 	public void join(Sample sample);
-	public void login(Sample sample);
+	public String login(Sample sample);
 	public Sample[] list();
+	public String existid(Sample sample);
+	public String Searchid(Sample sample);
+	public String Cntid(Sample sample);
+	public String namesearch(Sample sample);
+	public Sample[] Flist();
+	public String mypage(Sample sample);
 }
 /*
  * 요구사항 (기능정의)
  * <사용자기능>
  * 1. 회원가입
- * 2. 마이페이지
+ * 2. 로그인
  * 3. 비번 수정
  * 4. 회원탈퇴
  * 5. 아이디 존재 체크
- * 6. 로그인
+ * 6. 마이페이지
  * **********
  * <관리자기능>
- * 1. 회원목록
- * 2. 아이디검색
- * 3. 이름검색
- * 4. 전체 회원수
+ * 7. 회원목록
+ * 8. 아이디검색
+ * 9. 이름검색
+ * 10. 전체 회원수
  * */
--- sample/SampleServiceImpl.java
+++ sample/SampleServiceImpl.java
@@ -1,5 +1,8 @@
 package sample;
 
+import java.util.Scanner;
+
+
 public class SampleServiceImpl implements SampleService{
 	private Sample[] samples; // 배열
 	private int count;
@@ -16,21 +19,84 @@ public class SampleServiceImpl implements SampleService{
 	}
 
 	@Override
-	public void login(Sample sample) {
+	public String login(Sample sample) { // 로그인 
+		String result = "FAIL";
 		for(int i=0; i<count; i++) {
 			if(sample.getUserid().equals(samples[i].getUserid())
 					&&
 					sample.getPassword().equals(samples[i].getPassword())) {
-				System.out.println("로그인 성공");
+					result = "SUCCESS";
 				break;
-			}else {
-				System.out.println("로그인 실패");
 			}
 		}
+		return result;
 	}
-	@Override
+	@Override // 회원목록
 	public Sample[] list() {
 		return samples;
 	}
 
+	@Override // 아이디 체크
+	public String existid(Sample sample) {
+		System.out.println("중복체크할 아이디: "+sample.getUserid());
+		String result = "중복없음";
+		for(int i=0; i<count; i++) {
+			if(sample.getUserid().equals(samples[i].getUserid())) {
+				result = "중복된 아이디";
+			}
+		}
+		return result;
+	}
+
+	@Override // 아이디 검색
+	public String Searchid(Sample sample) {
+		System.out.println("검색할 아이디: "+sample.getUserid());
+		String result = "아이디 없음";
+		for(int i=0; i<count; i++) {
+			if(sample.getUserid().equals(samples[i].getUserid())) {
+				result = "아이디 있음";
+			}
+		}
+		return result;
+	}
+
+	@Override // 회원탈퇴
+	public String Cntid(Sample sample) {
+		System.out.println("회원탈퇴 아이디 입력: ");
+		int idx = -1;
+		String delid = sample.getUserid();
+		System.out.println("비밀번호 입력: ");
+		String delpw = sample.getUserid();
+		for(int i=0; i<count; i++) {
+			
+				idx = i;
+			}
+		
+		return null;
+	}
+
+	@Override // 이름검색
+	public String namesearch(Sample sample) {
+		System.out.println("검색할 이름 : "+sample.getName());
+		String result = "등록되지 않은 이름입니다.";
+		for(int i=0; i<count; i++) {
+			if(sample.getName().equals(samples[i].getName())) {
+				result = "등록된 이름입니다.";
+			}
+		}
+		return result;
+	}
+
+	@Override // 전체회원 검색 
+	public Sample[] Flist() {
+		return Flist();
+	}
+
+	@Override
+	public String mypage(Sample sample) {
+		System.out.println("아이디 :"+sample.getUserid());
+		return null;
+	}
 }
+
+
